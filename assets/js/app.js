d4cb65d224fee4fc492de4ee82f7a7782b4827a5
James Ward <james@notjam.es>
2014-10-24 02:43:35 -0400

    make it stop please

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	if (window.localStorage) {
+		// I am going to jump in front of a bus if I hear "TEAM PEGGLE PRESENTS" again
+
+		// Why use localStorage?  Because I really need this to stick or else comments re:bus
 		try {
 			var shouldStfu = JSON.parse(window.localStorage.getItem('stfu'));
 		} catch (e) {
 			var shouldStfu = false;
 		}
 
-		if (window.location.hash === '#stfu') {
-			shouldStfu = !shouldStfu;
-
-			// localstorage doesn't store
-			window.localStorage.setItem(
-				'stfu',
-				JSON.stringify(shouldStfu)
-			);
-		}
-
 		if (shouldStfu) {
-			// I swear I am so tired of that now
 			var audio = document.getElementById('audio-intro');
 
 			if (audio) {
 				audio.muted = true;
 			}
+		} else {
+			// localstorage doesn't store anything but strings
+			window.localStorage.setItem(
+				'stfu',
+				JSON.stringify(true)
+			);
 		}
 	}
 
