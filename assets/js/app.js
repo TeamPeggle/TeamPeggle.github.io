947f765d381ec5bdf7ff0e797efa217eb744ad07
James Ward <james@notjam.es>
2018-05-10 12:02:55 -0400

    sunii removed the audio intro so remove related JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	if (window.localStorage) {
-		// I am going to jump in front of a bus if I hear "TEAM PEGGLE PRESENTS" again
-
-		// Why use localStorage?  Because I really need this to stick or else comments re:bus
-		try {
-			var shouldStfu = JSON.parse(window.localStorage.getItem('stfu'));
-		} catch (e) {
-			var shouldStfu = false;
-		}
-
-		if (shouldStfu) {
-			var audio = document.getElementById('audio-intro');
-
-			if (audio) {
-				audio.muted = true;
-			}
-		} else {
-			// localstorage doesn't store anything but strings
-			window.localStorage.setItem(
-				'stfu',
-				JSON.stringify(true)
-			);
-		}
-	}
-
 	if (fartscroll) {
 		fartscroll(800);
 	}
